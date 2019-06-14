13a12e8a06803c9b64827accb32f0e4cf2830ff4
Rutvik Katuri <rutvik.katuri@iprotechs.com>
2019-06-14 20:02:59 +0530

    Created Configfiles and Written custom sequelize configuration file

---
+++ books/ConfigFiles/sequelize.js
@@ -0,0 +1,61 @@
+//Sequelize Class
+class CustomSequelize {
+    //Constructor
+    constructor(Sequelize, SynchForce, Authenticate, DB_NAME, DB_USERNAME, DB_PASSWORD, DB_HOST, DB_DIALECT, DB_MAX_POOL, DB_MIN_POOL, DB_ACQUIRE_POOL, DB_IDLE_POOL) {
+        this.Sequelize = Sequelize
+        this.SynchForce = SynchForce
+        this.Authenticate = Authenticate
+        this.DB_NAME = DB_NAME
+        this.DB_USERNAME = DB_USERNAME
+        this.DB_PASSWORD = DB_PASSWORD
+        this.DB_HOST = DB_HOST
+        this.DB_DIALECT = DB_DIALECT
+        this.DB_MAX_POOL = DB_MAX_POOL
+        this.DB_MIN_POOL = DB_MIN_POOL
+        this.DB_ACQUIRE_POOL = DB_ACQUIRE_POOL
+        this.DB_IDLE_POOL = DB_IDLE_POOL
+    }
+    //Init
+    init() {
+        if (this.Authenticate) {
+            this.authentication()
+        } else {
+            this.synchronization()
+        }
+    }
+    //This method is used for sequelize connection setup
+    sequelize = new this.Sequelize(this.DB_NAME, this.DB_USERNAME, this.DB_PASSWORD, {
+        host: this.DB_HOST,
+        dialect: this.DB_DIALECT || 'mysql',
+        operatorsAliases: false,
+        pool: {
+            max: this.DB_MAX_POOL || 3,
+            min: this.DB_MIN_POOL || 2,
+            acquire: this.DB_ACQUIRE_POOL || 30000,
+            idle: this.DB_IDLE_POOL || 10000
+        }
+    })
+    //Test the connection by trying to authenticate Aliases: validate
+    authentication() {
+        this.sequelize
+            .authenticate()
+            .then(() => {
+                console.log('Connection has been established successfully.');
+            })
+            .catch(err => {
+                console.error('Unable to connect to the database:', err.message);
+            })
+    }
+    //Sync all defined models to the DB.
+    synchronization() {
+        this.sequelize.sync({ force: this.SynchForce })
+            .then(() => {
+                console.log(`Database & tables created!`)
+            })
+            .catch(error => {
+                console.log("Table Creation Failed & Error is", error.message)
+            })
+    }
+}
+//Export sequelize connection                        
+export default CustomSequelize
--- books/book/book.model.js
+++ books/book/book.model.js
@@ -1,22 +1,34 @@
-// //Import Modules or Paths
-// import Sequelize from 'sequelize'
-// //import {sequelize} from "../app"
-// import * as Constants from '../app'
-// //Create a Schema
-// const BookSchema = Constants.sequelize.define('book', {
-//     id: {
-//         type: Sequelize.INTEGER,
-//         primaryKey: true,
-//         autoIncrement: true
-//     },
-//     name: {
-//         type: Sequelize.STRING,
-//         allowNull: false
-//     },
-//     customer_id: {
-//         type: Sequelize.INTEGER,
-//         allowNull:false
-//     }
-// });
+//Import Modules or Paths
+import Sequelize from 'sequelize'
+import CustomSequelize from '../ConfigFiles/sequelize'
+//CUSTOM SEQUELIZE
+const sequelize = new CustomSequelize(Sequelize,true,false,
+    process.env.DB_NAME,
+    process.env.DB_USERNAME,
+    process.env.DB_PASSWORD,
+    process.env.DB_HOST,
+    process.env.DB_DIALECT,
+    process.env.DB_MAX_POOL,
+    process.env.DB_MIN_POOL,
+    process.env.DB_ACQUIRE_POOL,
+    process.env.DB_IDLE,
+)
+sequelize.init()
+//Create a Schema
+const BookSchema = sequelize.sequelize.define('book', {
+    id: {
+        type: Sequelize.INTEGER,
+        primaryKey: true,
+        autoIncrement: true
+    },
+    name: {
+        type: Sequelize.STRING,
+        allowNull: false
+    },
+    customer_id: {
+        type: Sequelize.INTEGER,
+        allowNull: false
+    }
+});
 
-// export default BookSchema
+export default BookSchema
