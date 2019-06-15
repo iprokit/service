fc7df83ba46247129489a543b3e5dad9c457423a
Rutvik Katuri <rutvik.katuri@iprotechs.com>
2019-06-14 22:55:32 -0400

    get the mico_sdk working.

--- customers/router.js
+++
@@ -1,8 +0,0 @@
-//Import modules or path's
-import Router from 'express'
-import customerRoutes from './customer/customer.routes'
-//Create Router Instance
-const router = new Router()
-customerRoutes(router)
-//Export router
-export default router
