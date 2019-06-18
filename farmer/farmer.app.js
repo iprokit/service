0743cc3e3cf278516cfcf002279980f17591ae34
Rutvik Katuri <rutvik.katuri@iprotechs.com>
2019-06-18 11:08:57 +0530

    Fixed models issue and code cleanup.

--- farmer/farmer.app.js
+++ farmer/farmer.app.js
@@ -14,32 +14,31 @@ var microService = new MicroService({ name: "farmer",
         name: 'IPRO_AQU_ECS_VER4_6',
         username: 'microaqu',
         password: 'iPr0tech$2020',
-        timezone: '+5:30',
-        auth: true,
-        force: false}
+        timezone: '+5:30'
+    }
     });
 
 var sequelize = microService.getSequelize();
 
-//Need to move the below schema into a model
-var farmerModel = sequelize.define('farmer',{
-    id: {
-        type: DataTypes.INTEGER(6),
-        primaryKey: true,
-        autoIncrement: true
-    },
-    first_name: {
-        type: DataTypes.STRING(20),
-        allowNull: false
-    },
-    last_name: {
-        type: DataTypes.STRING(20),
-        allowNull: true
-    }
-});
+// //Need to move the below schema into a model
+// var farmerModel = sequelize.define('farmer',{
+//     id: {
+//         type: DataTypes.INTEGER(6),
+//         primaryKey: true,
+//         autoIncrement: true
+//     },
+//     first_name: {
+//         type: DataTypes.STRING(20),
+//         allowNull: false
+//     },
+//     last_name: {
+//         type: DataTypes.STRING(20),
+//         allowNull: true
+//     }
+// });
 
-// var farmerModel = new FarmerModel();
-// farmerModel.init(sequelize);
+var farmerModel = new FarmerModel();
+farmerModel.init(sequelize);
 
 var farmerController = new FarmerController(farmerModel);
 
