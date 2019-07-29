7c11a0c826b573dafb41f3dcf37d5e47f0520cdf
Rutvik Katuri <rutvik.katuri@iprotechs.com>
2019-07-29 05:10:44 -0400

    Exported file utility.

--- src/index.ts
+++ src/index.ts
@@ -2,3 +2,4 @@
 export { default as MicroService } from './app';
 export { default as Controller } from './controller';
 export { default as RDSModel, Op } from './db.rds.model';
+export { default as FileUtility } from './file.utility';
