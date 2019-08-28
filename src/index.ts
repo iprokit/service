61329b5f38090c4bc340cd9fc00887847713414a
Rutvik Katuri <rutvik.katuri@iprotechs.com>
2019-08-28 19:10:54 -0400

    Removed unused classes from index.

--- src/index.ts
+++ src/index.ts
@@ -2,8 +2,6 @@
 import MicroService from './app';
 
 //Exports
-export { default as FileUtility } from './file.utility';
-export { default as DockerUtility } from './docker.utility';
 export { default as Controller } from './controller';
 export { default as RDBModel, Op } from './db.rdb.model';
 export { Get, Post, Put, Delete } from './app';
