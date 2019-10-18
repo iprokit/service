c4dda3f3c1da66cb2a0e30cda2c4085c1cc51b18
Rutvik Katuri <rutvik.katuri@iprotechs.com>
2019-10-17 20:32:37 -0400

    Merged commClient into commMesh.

--- src/index.ts
+++ src/index.ts
@@ -4,8 +4,7 @@ import MicroService from './microservice';
 //Exports
 export { default as Gateway } from './gateway';
 export { Publisher } from './comm.broker';
-export { getService } from './comm.mesh';
-export { ServiceUnavailableError } from './comm.client';
+export { getService, ServiceUnavailableError } from './comm.mesh';
 export { default as Controller } from './controller';
 export { default as RDBModel, Op } from './db.rdb.model';
 export { default as NoSQLModel } from './db.nosql.model';
