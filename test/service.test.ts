485f3c7c9b21eaad3a529c32ac77d817f187b064
Rutvik Katuri <rutvik.katuri@iprotechs.com>
2020-07-30 22:16:45 -0400

    Added express tests.

--- test/service.test.ts
+++ test/service.test.ts
@@ -148,9 +148,7 @@ mocha.describe('Service Test', () => {
         silentLog(service);
 
         mocha.after((done) => {
-            service.stop(() => {
-                done();
-            });
+            service.stop(done);
         });
 
         mocha.it('should emit starting/started event', (done) => {
@@ -205,103 +203,6 @@ mocha.describe('Service Test', () => {
         });
     });
 
-    mocha.describe('Service Routes Test', () => {
-        const service = new Service({ name: 'HeroSVC', version: '1.0.0', logPath: logPath });
-        silentLog(service);
-
-        mocha.before((done) => {
-            service.start(done);
-        });
-
-        mocha.after((done) => {
-            service.stop(() => {
-                done();
-            });
-        });
-
-        it('should execute GET(/health) and receive body(JSON)', (done) => {
-            httpRequest('get', '/health', {}, true, (response, error) => {
-                assert.deepStrictEqual(response.statusCode, HttpStatusCodes.OK);
-                assert.deepStrictEqual(response.body.name, 'HeroSVC');
-                assert.deepStrictEqual(response.body.version, '1.0.0');
-                assert.deepStrictEqual(response.body.httpServer, true);
-                assert.deepStrictEqual(response.body.scpServer, true);
-                assert.deepStrictEqual(response.body.discovery, true);
-                assert.deepStrictEqual(response.body.healthy, true);
-                done(error);
-            });
-        });
-
-        it('should execute GET(/report) and receive body(JSON)', (done) => {
-            httpRequest('get', '/report', {}, true, (response, error) => {
-                assert.deepStrictEqual(response.statusCode, HttpStatusCodes.OK);
-                assert.notDeepStrictEqual(response.body.service, undefined);
-                assert.notDeepStrictEqual(response.body.endpoints, undefined);
-                assert.notDeepStrictEqual(response.body.actions, undefined);
-                assert.notDeepStrictEqual(response.body.mesh, undefined);
-                assert.notDeepStrictEqual(response.body.serviceRegistry, undefined);
-                done(error);
-            });
-        });
-    });
-
-    mocha.describe('Default Middleware Test', () => {
-        const service = new Service({ name: 'HeroSVC', version: '1.0.0', logPath: logPath });
-        silentLog(service);
-
-        mocha.before((done) => {
-            service.start(done);
-        });
-
-        mocha.after((done) => {
-            service.stop(() => {
-                done();
-            });
-        });
-
-        //Server
-        service.get('/hero', (request, respose) => {
-            respose.status(HttpStatusCodes.OK).send(request.body);
-        });
-
-        service.get('/hero/timeout', (request, respose) => {
-            respose.setTimeout(100);
-            setTimeout(() => {
-                try {
-                    respose.status(HttpStatusCodes.OK).send(request.body);
-                } catch (error) {
-                    assert.deepStrictEqual(error.code, 'ERR_HTTP_HEADERS_SENT');
-                }
-            }, 200);
-        });
-
-        //Client
-        it('should execute GET(/hero) and receive body(JSON) with CORS support', (done) => {
-            httpRequest('get', '/hero', { hero: 'Iron Man' }, true, (response, error) => {
-                assert.deepStrictEqual(response.headers['access-control-allow-origin'], '*');
-                assert.deepStrictEqual(response.statusCode, HttpStatusCodes.OK);
-                assert.deepStrictEqual(response.body, { hero: 'Iron Man' });
-                done(error);
-            });
-        });
-
-        it('should execute POST(/hero) and receive Error(Not Found)', (done) => {
-            httpRequest('post', '/hero', '', false, (response, error) => {
-                assert.deepStrictEqual(response.statusCode, HttpStatusCodes.NOT_FOUND);
-                assert.deepStrictEqual(response.body, 'Not Found');
-                done(error);
-            });
-        });
-
-        it('should execute POST(/hero) and receive Error(Service Unavailable)', (done) => {
-            httpRequest('get', '/hero/timeout', '', false, (response, error) => {
-                assert.deepStrictEqual(response.statusCode, HttpStatusCodes.SERVICE_UNAVAILABLE);
-                assert.deepStrictEqual(response.body, 'Service Unavailable');
-                done(error);
-            });
-        });
-    });
-
     mocha.describe('Service Registry Test', () => {
         mocha.describe('Availability Test', () => {
             const shield = new Service({ name: 'Shield', version: '1.0.0', logPath: logPath, httpPort: 3001, scpPort: 6001 });
@@ -312,13 +213,11 @@ mocha.describe('Service Test', () => {
 
             mocha.after((done) => {
                 shield.stop(() => {
-                    hydra.stop(() => {
-                        done();
-                    });
+                    hydra.stop(done);
                 });
             });
 
-            mocha.it('Service(hydra) should be available to Service(shield)', (done) => {
+            mocha.it('hydra should be available to shield', (done) => {
                 shield.on('available', (remoteService: RemoteService) => {
                     assert.deepStrictEqual(remoteService.name, 'Hydra');
                     assert.deepStrictEqual(remoteService.alias, undefined);
@@ -332,8 +231,8 @@ mocha.describe('Service Test', () => {
                 hydra.start(() => {
                     shield.start();
                 });
-            });
-        }).timeout(1000 * 5);
+            }).timeout(1000 * 5);
+        });
 
         mocha.describe('Unavailability Test', () => {
             const shield = new Service({ name: 'Shield', version: '1.0.0', logPath: logPath, httpPort: 3001, scpPort: 6001 });
@@ -344,13 +243,11 @@ mocha.describe('Service Test', () => {
 
             mocha.before((done) => {
                 shield.start(() => {
-                    hydra.start(() => {
-                        done();
-                    });
+                    hydra.start(done);
                 });
             });
 
-            mocha.it('Service(hydra) should be unavailable to Service(shield)', (done) => {
+            mocha.it('hydra should be unavailable to shield', (done) => {
                 shield.on('unavailable', (remoteService: RemoteService) => {
                     assert.deepStrictEqual(remoteService.name, 'Hydra');
                     assert.deepStrictEqual(remoteService.defined, false);
@@ -365,24 +262,153 @@ mocha.describe('Service Test', () => {
                 hydra.stop(() => {
                     shield.stop();
                 });
-            });
-        }).timeout(1000 * 5);
+            }).timeout(1000 * 5);
+        });
     });
 
-    mocha.describe('Creation Test', () => {
-        mocha.it('should create route', () => {
-            const service = new Service({ name: 'HeroSVC', version: '1.0.0', logPath: logPath });
-            const route = service.createRouter('/hero');
+    mocha.describe('Express Test', () => {
+        const service = new Service({ name: 'HeroSVC', version: '1.0.0', logPath: logPath });
+        silentLog(service);
 
-            assert.notDeepStrictEqual(route, undefined);
-            assert.deepStrictEqual(service.routes.length, 1);
+        mocha.before((done) => {
+            service.start(done);
+        });
+
+        mocha.after((done) => {
+            service.stop(done);
+        });
+
+        mocha.describe('Creation + Middleware Test', () => {
+            const heroRouter = service.createRouter('/hero');
+
+            //Before Routes.
+            heroRouter.use('/', (request, response, next) => {
+                request.body.hero = true;
+                next();
+            });
+
+            //Routes.
+            heroRouter.get('/', (request, response) => {
+                assert.deepStrictEqual(request.body.hero, true);
+
+                response.status(HttpStatusCodes.OK).send({ heros: ['Captain America', 'Iron Man', 'Black Widow'] });
+            }).post('/', (request, response) => {
+                assert.deepStrictEqual(request.body.hero, true);
+
+                response.status(HttpStatusCodes.CREATED).send({ hero: 'Vision' });
+            }).put('/', (request, response) => {
+                assert.deepStrictEqual(request.body.hero, true);
+
+                response.status(HttpStatusCodes.OK).send({ hero: 'Thor' });
+            }).get('/snap', (request, response) => {
+                assert.deepStrictEqual(request.body.hero, true);
+
+                response.setTimeout(100);
+                setTimeout(() => {
+                    try {
+                        response.status(HttpStatusCodes.OK).send(request.body);
+                    } catch (error) {
+                        assert.deepStrictEqual(error.code, 'ERR_HTTP_HEADERS_SENT');
+                    }
+                }, 200);
+            });
+
+            //After Routes.
+            heroRouter.use('/', (request, response) => {
+                assert.deepStrictEqual(request.body.hero, true);
+
+                response.status(HttpStatusCodes.NOT_FOUND).send('No Hero Route Found');
+            });
+
+            //Client
+            mocha.it('should execute GET(/hero) and receive body(JSON) with CORS support', (done) => {
+                httpRequest('get', '/hero', {}, true, (response, error) => {
+                    assert.deepStrictEqual(response.headers['access-control-allow-origin'], '*');
+                    assert.deepStrictEqual(response.statusCode, HttpStatusCodes.OK);
+                    assert.deepStrictEqual(response.body, { heros: ['Captain America', 'Iron Man', 'Black Widow'] });
+                    done(error);
+                });
+            });
+
+            mocha.it('should execute POST(/hero) and receive body(JSON) with CORS support', (done) => {
+                httpRequest('post', '/hero', {}, true, (response, error) => {
+                    assert.deepStrictEqual(response.headers['access-control-allow-origin'], '*');
+                    assert.deepStrictEqual(response.statusCode, HttpStatusCodes.CREATED);
+                    assert.deepStrictEqual(response.body, { hero: 'Vision' });
+                    done(error);
+                });
+            });
+
+            mocha.it('should execute PUT(/hero) and receive body(JSON) with CORS support', (done) => {
+                httpRequest('put', '/hero', {}, true, (response, error) => {
+                    assert.deepStrictEqual(response.headers['access-control-allow-origin'], '*');
+                    assert.deepStrictEqual(response.statusCode, HttpStatusCodes.OK);
+                    assert.deepStrictEqual(response.body, { hero: 'Thor' });
+                    done(error);
+                });
+            });
+
+            mocha.it('should execute GET(/hero/snap) and receive Error(Service Unavailable) with CORS support', (done) => {
+                httpRequest('get', '/hero/snap', '', false, (response, error) => {
+                    assert.deepStrictEqual(response.headers['access-control-allow-origin'], '*');
+                    assert.deepStrictEqual(response.statusCode, HttpStatusCodes.SERVICE_UNAVAILABLE);
+                    assert.deepStrictEqual(response.body, 'Service Unavailable');
+                    done(error);
+                });
+            });
+            
+            mocha.it('should execute DELETE(/hero) and receive Error(Not Found) with CORS support', (done) => {
+                httpRequest('delete', '/hero', '', false, (response, error) => {
+                    assert.deepStrictEqual(response.headers['access-control-allow-origin'], '*');
+                    assert.deepStrictEqual(response.statusCode, HttpStatusCodes.NOT_FOUND);
+                    assert.deepStrictEqual(response.body, 'No Hero Route Found');
+                    done(error);
+                });
+            });
+
+            mocha.it('should execute GET(/) and receive Error(Not Found) with CORS support', (done) => {
+                httpRequest('get', '/', '', false, (response, error) => {
+                    assert.deepStrictEqual(response.headers['access-control-allow-origin'], '*');
+                    assert.deepStrictEqual(response.statusCode, HttpStatusCodes.NOT_FOUND);
+                    assert.deepStrictEqual(response.body, 'Not Found');
+                    done(error);
+                });
+            });
+        });
+
+        mocha.describe('Default Routes Test', () => {
+            mocha.it('should execute GET(/health) and receive body(JSON)', (done) => {
+                httpRequest('get', '/health', {}, true, (response, error) => {
+                    assert.deepStrictEqual(response.headers['access-control-allow-origin'], '*');
+                    assert.deepStrictEqual(response.statusCode, HttpStatusCodes.OK);
+                    assert.deepStrictEqual(response.body.name, 'HeroSVC');
+                    assert.deepStrictEqual(response.body.version, '1.0.0');
+                    assert.deepStrictEqual(response.body.httpServer, true);
+                    assert.deepStrictEqual(response.body.scpServer, true);
+                    assert.deepStrictEqual(response.body.discovery, true);
+                    assert.deepStrictEqual(response.body.healthy, true);
+                    done(error);
+                });
+            });
+
+            mocha.it('should execute GET(/report) and receive body(JSON)', (done) => {
+                httpRequest('get', '/report', {}, true, (response, error) => {
+                    assert.deepStrictEqual(response.headers['access-control-allow-origin'], '*');
+                    assert.deepStrictEqual(response.statusCode, HttpStatusCodes.OK);
+                    assert.notDeepStrictEqual(response.body.service, undefined);
+                    assert.notDeepStrictEqual(response.body.endpoints, undefined);
+                    assert.notDeepStrictEqual(response.body.actions, undefined);
+                    assert.notDeepStrictEqual(response.body.mesh, undefined);
+                    assert.notDeepStrictEqual(response.body.serviceRegistry, undefined);
+                    done(error);
+                });
+            });
         });
     });
 });
 
 /**
  * TODO: 
- * Use, all, get, post, put, delete - creation and call test. With use before and after test.
  * Reply, define, broadcast - creation and call test.
  * Discover - creation and call test.
  * Proxy test
