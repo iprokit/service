0531a061763a36a60b742c2ff06b1116380edf88
Rutvik Katuri <rutvik.katuri@iprotechs.com>
2020-08-06 00:31:43 -0400

    Updated package with released scp and discovery versions.

--- src/index.ts
+++ src/index.ts
@@ -7,12 +7,15 @@ export { default as HttpStatusCodes } from './http.statusCodes';
 
 //Export SCP
 export { default as Messenger } from './messenger';
-export { SocketError, ErrorReply } from '@iprotechs/scp';
+export { SocketError, ErrorReply, WriteError } from '@iprotechs/scp';
 
 //Export DB Components
-export { RDB, NoSQL } from './db.manager';
+export { RDB, NoSQL, ModelError, InvalidConnectionOptions } from './db.manager';
 export { default as RDBModel, RDBDataTypes, RDBOp } from './db.rdb.model';
 export { default as NoSQLModel, NoSQLDataTypes } from './db.nosql.model';
+
+//Export Service Components
+export { InvalidServiceOptions } from './service';
 
 //Export Micro Components
 export { mesh as Mesh, proxy as Proxy, controllers as Controllers, models as Models, messengers as Messengers, Entity, Get, Post, Put, Delete, Reply } from './micro';
