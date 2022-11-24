11eca9d38712ea84f82d92e7e6c2cda6e3d00f8c
Rutvik Katuri <rutvik.katuri@iprotechs.com>
2022-11-24 01:07:26 -0500

    Added test index.

---
+++ test/index.test.ts
@@ -0,0 +1,7 @@
+//Import Local.
+// import './micro.test';
+// import './service.test';
+// import './controller.test';
+// import './db.manager.test';
+// import './db.rdb.model.test';
+// import './db.nosql.model.test';
